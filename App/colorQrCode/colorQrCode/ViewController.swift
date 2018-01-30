f770b1fb06b6d503cbd038524608949cbcbb2741
xiepengxiang <xiepengxiang@365ime.com>
2018-01-30 17:19:03 +0800

    Brief: add App colorfuncQRCode
    Jira:
    Desc:
    Reviewer:
    Signed-off-by: Xie pengxiang <xiepengxiang@365ime.com>

---
+++ App/colorQrCode/colorQrCode/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  colorQrCode
+//
+//  Created by 谢鹏翔 on 2018/1/29.
+//  Copyright © 2018年 365ime. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let qrView = ColorfulQRCodeView(frame: CGRect(x: 50, y:50, width: 100, height: 100));
+        self.view.addSubview(qrView);
+        qrView.backgroundColor = UIColor.lightGray
+        
+        if let image = UIImage.init(named: "qrcode") {
+            qrView.setQRcodeImage(qrcodeImage: image)
+        }
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
