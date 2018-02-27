5c6b389988334df7d2671d4862865af39f0f8f58
xiepengxiang <xiepengxiang@365ime.com>
2018-02-27 15:38:36 +0800

    Brief: class instance & list
    Jira:
    Desc:
    Reviewer:
    Signed-off-by: Xie pengxiang <xiepengxiang@365ime.com>

--- runtime/runtime/main.m
+++ runtime/runtime/main.m
@@ -14,88 +14,153 @@
 int main(int argc, char * argv[]) {
     @autoreleasepool {
         
-        MyClass *myClass = [[MyClass alloc] init];
-        unsigned int outCount = 0;
-        Class cls = myClass.class;
-        
-        // 类名
-        NSLog(@"class name : %s", class_getName(cls));
-        NSLog(@"=============================");
-        
-        // 父类
-        NSLog(@"super class name : %s", class_getName(class_getSuperclass(cls)));
-        NSLog(@"=============================");
-        
-        // 是否元类
-        NSLog(@"MyClass is %@ a meta-class", (class_isMetaClass(cls)) ? @"" : @"not");
-        NSLog(@"=============================");
-        
-        Class meta_class = objc_getMetaClass(class_getName(cls));
-        NSLog(@"%s's meta-class is %s", class_getName(cls), class_getName(meta_class));
-        NSLog(@"=============================");
-        
-        // 成员变量
-        Ivar *ivars = class_copyIvarList(cls, &outCount);
-        for (int i = 0; i < outCount; i++) {
-            Ivar ivar = ivars[i];
-            NSLog(@"instace variable's name : %s at index : %d", ivar_getName(ivar), i);
-        }
-        free(ivars);
-        
-        Ivar string = class_getInstanceVariable(cls, "_string");
-        if (string != NULL) {
-            NSLog(@"instance variable %s", ivar_getName(string));
-        }
-        NSLog(@"=============================");
-        
-        // 属性操作
-        objc_property_t *properties = class_copyPropertyList(cls, &outCount);
-        for (int i = 0; i < outCount; i++) {
-            objc_property_t property = properties[i];
-            NSLog(@"property's name: %s", property_getName(property));
-        }
-        free(properties);
-        
-        objc_property_t array = class_getProperty(cls, "array");
-        if (array != NULL) {
-            NSLog(@"property %s", property_getName(array));
-        }
-        NSLog(@"=============================");
-        
-        // 方法操作
-        Method *methods = class_copyMethodList(cls, &outCount);
-        for (int i = 0; i < outCount; i++) {
-            Method method = methods[i];
-            NSLog(@"method's signature: %s", method_getName(method));
-        }
-        free(methods);
-        
-        Method method1 = class_getInstanceMethod(cls, @selector(method1));
-        if (method1 != NULL) {
-            NSLog(@"method %s", method_getName(method1));
-        }
-        
-        Method classMethod = class_getClassMethod(cls, @selector(classMethod1));
-        if (classMethod != NULL) {
-            NSLog(@"class method : %s", method_getName(classMethod));
-        }
-        NSLog(@"MyClass is%@ responsed to selector: method3WithArg1:arg2:", class_respondsToSelector(cls, @selector(method3WithArg1:arg2:)) ? @"" : @" not");
-        
-        IMP imp = class_getMethodImplementation(cls, @selector(method1));
-        imp();
-        NSLog(@"=============================");
-        
-        //协议
-        Protocol * __unsafe_unretained * protocols = class_copyProtocolList(cls, &outCount);
-        Protocol * protocol;
-        for (int i = 0; i < outCount; i++) {
-            protocol = protocols[i];
-            NSLog(@"protocol name : %s", protocol_getName(protocol));
-        }
-        NSLog(@"MyClass is%@ responsed to protocol %s", class_conformsToProtocol(cls, protocol) ? @"" : @" not", protocol_getName(protocol));
-        NSLog(@"=============================");
         
         
         return 0;
     }
+}
+
+void class_list_test()
+{
+    int numClasses;
+    Class *classes = NULL;
+    
+    numClasses = objc_getClassList(NULL, 0);
+    if (numClasses > 0) {
+        classes = (Class *)malloc(sizeof(Class) * numClasses);
+        numClasses = objc_getClassList(classes, numClasses);
+        
+        NSLog(@"number of classes: %d", numClasses);
+        
+        for (int i = 0; i < numClasses; i++) {
+            Class cls = classes[i];
+            NSLog(@"class name: %s", class_getName(cls));
+        }
+        
+        free(classes);
+    }
+}
+
+void class_instance_create_test()
+{
+    // 创建类实例
+    id theObject = class_createInstance(NSString.class, sizeof(unsigned));
+    
+    id str1 = [theObject init];
+    NSLog(@"%@", [str1 class]);
+    
+    id str2 = [[NSString alloc] init];
+    NSLog(@"%@", [str2 class]);
+}
+
+void class_create_dynamic_test()
+{
+    MyClass *myClass = [[MyClass alloc] init];
+    Class myclass = myClass.class;
+    
+    //动态创建类
+    Class cls = objc_allocateClassPair(MyClass.class, "MySubClass", 0);
+    
+    IMP imp_submethod1 = class_getMethodImplementation(myclass, @selector(method1));
+    IMP imp_submethod2 = class_getMethodImplementation(myclass, @selector(method2));
+    
+    class_addMethod(cls, @selector(submethod1), (IMP)imp_submethod1, "v@:");
+    class_replaceMethod(cls, @selector(method1), imp_submethod2, "v@:");
+    class_addIvar(cls, "_ivar1", sizeof(NSString *), log(sizeof(NSString *)), "i");
+    
+    objc_property_attribute_t type = {"T", "@\"NSString\""};
+    objc_property_attribute_t ownership = {"C", ""};
+    objc_property_attribute_t backingivar = {"V", "_ivar1"};
+    objc_property_attribute_t attrs[] = {type, ownership, backingivar};
+    
+    class_addProperty(cls, "property2", attrs, 3);
+    objc_registerClassPair(cls);
+    
+    id instance = [[cls alloc] init];
+    [instance performSelector:@selector(submethod1)];
+    [instance performSelector:@selector(method1)];
+}
+
+void class_method_protocol_test()
+{
+    MyClass *myClass = [[MyClass alloc] init];
+    unsigned int outCount = 0;
+    Class cls = myClass.class;
+    
+    // 类名
+    NSLog(@"class name : %s", class_getName(cls));
+    NSLog(@"=============================");
+    
+    // 父类
+    NSLog(@"super class name : %s", class_getName(class_getSuperclass(cls)));
+    NSLog(@"=============================");
+    
+    // 是否元类
+    NSLog(@"MyClass is %@ a meta-class", (class_isMetaClass(cls)) ? @"" : @"not");
+    NSLog(@"=============================");
+    
+    Class meta_class = objc_getMetaClass(class_getName(cls));
+    NSLog(@"%s's meta-class is %s", class_getName(cls), class_getName(meta_class));
+    NSLog(@"=============================");
+    
+    // 成员变量
+    Ivar *ivars = class_copyIvarList(cls, &outCount);
+    for (int i = 0; i < outCount; i++) {
+        Ivar ivar = ivars[i];
+        NSLog(@"instace variable's name : %s at index : %d", ivar_getName(ivar), i);
+    }
+    free(ivars);
+    
+    Ivar string = class_getInstanceVariable(cls, "_string");
+    if (string != NULL) {
+        NSLog(@"instance variable %s", ivar_getName(string));
+    }
+    NSLog(@"=============================");
+    
+    // 属性操作
+    objc_property_t *properties = class_copyPropertyList(cls, &outCount);
+    for (int i = 0; i < outCount; i++) {
+        objc_property_t property = properties[i];
+        NSLog(@"property's name: %s", property_getName(property));
+    }
+    free(properties);
+    
+    objc_property_t array = class_getProperty(cls, "array");
+    if (array != NULL) {
+        NSLog(@"property %s", property_getName(array));
+    }
+    NSLog(@"=============================");
+    
+    // 方法操作
+    Method *methods = class_copyMethodList(cls, &outCount);
+    for (int i = 0; i < outCount; i++) {
+        Method method = methods[i];
+        NSLog(@"method's signature: %s", method_getName(method));
+    }
+    free(methods);
+    
+    Method method1 = class_getInstanceMethod(cls, @selector(method1));
+    if (method1 != NULL) {
+        NSLog(@"method %s", method_getName(method1));
+    }
+    
+    Method classMethod = class_getClassMethod(cls, @selector(classMethod1));
+    if (classMethod != NULL) {
+        NSLog(@"class method : %s", method_getName(classMethod));
+    }
+    NSLog(@"MyClass is%@ responsed to selector: method3WithArg1:arg2:", class_respondsToSelector(cls, @selector(method3WithArg1:arg2:)) ? @"" : @" not");
+    
+    IMP imp = class_getMethodImplementation(cls, @selector(method1));
+    imp();
+    NSLog(@"=============================");
+    
+    //协议
+    Protocol * __unsafe_unretained * protocols = class_copyProtocolList(cls, &outCount);
+    Protocol * protocol;
+    for (int i = 0; i < outCount; i++) {
+        protocol = protocols[i];
+        NSLog(@"protocol name : %s", protocol_getName(protocol));
+    }
+    NSLog(@"MyClass is%@ responsed to protocol %s", class_conformsToProtocol(cls, protocol) ? @"" : @" not", protocol_getName(protocol));
+    NSLog(@"=============================");
 }
